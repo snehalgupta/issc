125ae206d360a6710faf7c7cb24a89134432ddd7
snehalgupta <snehal16201@iiitd.ac.in>
2016-11-12 03:43:19 +0530

    Yes! the adding field worked, now subtask has an additional field projectid

--- dbms/models.py
+++ dbms/models.py
@@ -3,6 +3,7 @@ from django.db import models
 # Create your models here.
 
 class Subtask(models.Model):
+      projectid=models.CharField(max_length=50, default = "primenos")
       taskid=models.CharField(max_length=20)
       status=models.CharField(max_length=20)
       task=models.CharField(max_length=10)
