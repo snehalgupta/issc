936a99151fdab06a574cc3417f0d0ea2ae459c0c
snehalgupta <snehal16201@iiitd.ac.in>
2016-11-17 06:47:17 +0530

    finished the primenos app.Defined function allot_time to turn the TA tasks to NA when the user does not respond for a long time.

--- dbms/views.py
+++ dbms/views.py
@@ -1,22 +1,61 @@
-
+import datetime
 from .models import Subtask
 from django.http import HttpResponse,JsonResponse
 import zipfile
-#import subprocess
+import subprocess
+
+def allot_time():
+	file = open('./dbms/time.txt', 'r')
+	l=file.readlines()
+	now = datetime.datetime.now()
+	c=[]
+	file.close()
+	for k in range(len(l)):
+		i=l[k]
+		prid = i[:i.find('/')]
+		tid = i[i.find('/')+1:i.find(' -')]
+		time = i[i.find('> ')+2:]
+		time = time.rstrip('\n')
+		t = datetime.datetime.strptime(time, "%Y-%m-%d %H:%M:%S.%f")
+		if (now-t)>datetime.timedelta(0,60,0):
+			j = Subtask.objects.filter(projectid=prid,taskid=tid)
+			subt = j[0]
+			subt.status="NA"
+			subt.save()
+			c.append(k)
+	indcompen = 0
+	for z in c:
+		l.pop(z-indcompen)
+		indcompen+=1
+	file=open('./dbms/time.txt', 'w')
+	for a in l:
+		file.write(a)
+	file.close()
 
 # Create your views here.
-
-
-
-def ExportSubtask(request):
-	subtasks = Subtask.objects.filter(status="NA")
+def ExportSubtask(request, prid):
+	pid = prid
+	subtasks = Subtask.objects.filter(projectid=str(pid), status='NA')
 	if len(subtasks)==0:
-		response = JsonResponse({'alltasks':'done'})
-		return response
+		allot_time()
+		subtasks = Subtask.objects.filter(projectid=str(pid), status='NA')
+
+	if len(subtasks)==0:
+		subtaskta = Subtask.objects.filter(projectid=str(pid), status='TA')
+		if len(subtaskta)>0:
+			response = JsonResponse({'check after':'75 seconds','prid':prid})
+			return response
+		else:
+			response = JsonResponse({'alltasks':'done','prid':prid})
+			return response
 	else:
 		i = subtasks[0]
 		i.status="TA"
 		i.save()
+		t=datetime.datetime.now()
+		time=open('./dbms/time.txt', 'a')
+		time.write(i.projectid+"/"+i.taskid+" -> "+str(t)+'\n')
+		time.close()
 		response = JsonResponse({'prid':str(i.projectid),'taskid':str(i.taskid),'task':str(i.task)})
 		return response
 
@@ -47,14 +86,34 @@ def Download(request):
     response['Content-Disposition'] = 'attachment; filename="%s"' % 'setup_primenos.zip'
     return response
 
+
+
 def Change(request, prid, tid):
-    pid=prid
-    id=tid
-    subtasks=list(Subtask.objects.filter(projectid=pid, taskid=id))
-    i=subtasks[0]
-    i.status='A'
-    i.save()
-    #response = JsonResponse({'prid':str(i.projectid),'taskid':str(i.taskid),'task':str(i.task)})
-	#return response
-	
-	
+	pid=prid
+	id=tid
+	subtasks=list(Subtask.objects.filter(projectid=pid, taskid=id))
+	i=subtasks[0]
+	i.status='A'
+	l=open('./dbms/time.txt', 'r')
+	m=l.readlines()
+	l.close()
+	'''l=open('./dbms/time.txt', 'w')
+	l.close()'''
+	for j in range(len(m)):
+		#k=m[j]
+		#prid2 = k[:k.find('/')]
+		#tid2 = k[k.find('/')+1:k.find(' -')]
+		#if(pid!=prid2 and id!=tid2):
+		#	l=open('./dbms/time.txt', 'a')
+		#	l.write(k)
+		#	l.close()
+		if (prid+'/'+tid) in m[j]:
+			m.pop(j)
+			break
+
+	file=open('./dbms/time.txt', 'w')
+	for a in m:
+		file.write(a)
+	file.close()
+	i.save()
+
